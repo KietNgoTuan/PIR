f4fb61a0cf91443f41c6578cd9f468e697dce3bc
matthrx <matthieu_upmc@outlook.fr>
2019-05-29 18:15:40 +0200

    BROADCAST UDP Video in tmp (mp4) Encodage/Decodage (avec dépadding)

--- client/client.py
+++ client/client.py
@@ -1,16 +1,16 @@
-import socket as s
+import socket
 import os
-import subprocess
-import sys
-import memcache
-import threading
+import hashlib
 import tempfile
 
 HOST ="127.0.0.1" # Must be changed with the real server IP address
 PORT=25555
+BROADCAST_PORT = 40000
 DIR_TEMP_NAME = "PIRCaching"
+ALL_TEMP_FILES = list()
 
-client= s.socket(s.AF_INET,s.SOCK_STREAM)
+
+client= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST,PORT))
 client.settimeout(1)
 full_data = str()
@@ -24,6 +24,106 @@ if DIR_TEMP_NAME not in os.listdir(tempfile.gettempdir()):
 tempfile.tempdir = tempfile.gettempdir()+"/"+DIR_TEMP_NAME
 print(tempfile.gettempdir())
 
+"""
+Code to check all files stored in temp memory
+"""
+
+for file in os.listdir(tempfile.gettempdir()):
+    ALL_TEMP_FILES.append(file.split('.')[0])
+
+print(ALL_TEMP_FILES)
+
+receive_broadcast = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+receive_broadcast.bind(('', BROADCAST_PORT))
+receive_broadcast.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+receive_broadcast.settimeout(1)
+
+hash = hashlib.md5()
+
+
+
+def padding(f1,f2):
+    size=0
+    dif = os.stat(f1).st_size-os.stat(f2).st_size
+    print(dif)
+    tab = bytearray(abs(dif))
+    for i in range(abs(dif)):
+        tab[i]=0
+    if dif == 0:
+        size=0
+    if dif >0:
+        size=os.stat(f2).st_size
+        file2 = open(f2, 'ab+')
+        file2.write(tab)
+        file2.close()
+    elif dif<0:
+        size=os.stat(f1).st_size
+        file1 = open(f1, 'ab+')
+        file1.write(tab)
+        file1.close()
+    return size
+
+def depadding(f,size):
+    fi=open(f,"rb+")
+    tab=fi.read()
+    temp=tab[:size]
+    fi.seek(0)
+    fi.truncate()
+    fi.write(temp)
+    fi.close()
+def couper(f,size):
+        fi=open(f,"rb+")
+        tab=fi.read()
+
+        temp=tab[size:]
+        fi.seek(0)
+        fi.truncate()
+        fi.write(temp)
+        fi.close()
+
+
+def encode(f1, f2, f3):
+    smallsize = padding(f1, f2)
+    s = "%032d" % int(bin(smallsize)[2:])
+    tabsize = bytearray(s.encode())
+    size = os.stat(f1).st_size
+    result = bytearray(size)
+    with open(f1, 'rb') as file1:
+        with open(f2, 'rb') as file2:
+            with open(f3, 'wb') as file_out:
+                fi1 = bytearray(file1.read())
+                fi2 = bytearray(file2.read())
+                for byte1 in range(size):
+                    result[byte1] = (fi1[byte1] ^ fi2[byte1])
+                if smallsize == 0:
+                    tab = result
+                else:
+                    tab = tabsize + result
+                file_out.write(tab)
+                print(os.stat(f3).st_size)
+                return f3
+
+
+def decode(f1,f2,f3):
+    fi=open(f1,"rb")
+    temp=fi.read()[:32]
+    tab=temp.decode()
+    print(int(tab,2))
+    fi.close()
+    size= int(tab,2)
+    couper(f1,32)
+    dif=os.stat(f1).st_size-os.stat(f2).st_size
+    print(os.stat(f1).st_size)
+    print(os.stat(f2).st_size)
+    if dif>0:
+        padding(f1,f2)
+        encode(f1,f2,f3)
+        depadding(f2,dif)
+        print("dsd")
+    else:
+        encode(f1,f2,f3)
+        depadding(f3,size)
+        print("sss")
 # try:
 #     def __run__cache():
 #         subprocess.call("..\memcached\memcached.exe")
@@ -38,26 +138,34 @@ print(tempfile.gettempdir())
 # cache = memcache.Client(["127.0.0.1:11211"], debug=0)
 
 while(True):
-    message = input("Fichier à télecharger : ")
-    if message == str():
+    plain_message = input("Fichier à télecharger : ")
+    hash.update(bytes(plain_message, "utf-8"))
+    hashed_message = hash.hexdigest()
+
+    if hashed_message in ALL_TEMP_FILES:
         print("Got it in cache")
     else:
 
-        client.send(message.encode('utf-8'))
-        data = client.recv(4096)
-
-        while (True):
-            full_data += str(data)
-            try:
-                data = client.recv(4096)
-            except s.timeout:
-                break
+        client.send(hashed_message.encode('utf-8'))
 
 
-        print("All data received")
-        print(full_data.split("key:")[1])
-        temp = tempfile.TemporaryFile(prefix=full_data.split("key:")[1], suffix="")
-        temp.write(bytes(full_data.split("key:")[0], "utf-8"))
+        data, _ = receive_broadcast.recvfrom(4096)
+
+        with open(tempfile.gettempdir()+"/"+hashed_message+".mp4", "wb") as mp4file:
+            while (True):
+                mp4file.write(data)
+                try:
+                    data,_ = receive_broadcast.recvfrom(4096)
+                except socket.timeout:
+                    receive_broadcast.close()
+                    break
+            mp4file.close()
+
+        # print(len(full_data))
+        # print("All data received")
+        # temp = tempfile.NamedTemporaryFile(prefix=hashed_message+'\'', suffix="", delete=False)
+        # temp.write(bytes(full_data, "utf-8"))
+        ALL_TEMP_FILES.append(hashed_message)
 
     # if(data.decode('utf-8') != "File not found" and data != str()):
     #     data = data.decode('utf-8').split("+")
--- server/server.py
+++ server/server.py
@@ -2,17 +2,99 @@ import socket
 import sys
 import os
 import threading
-from functools import partial
+import hashlib
+
 
 NB_CLIENT = 10
 PORT = 25555
+BROADCAST_PORT = 44444
+
+INDEX_VIDEOS = dict()
+
+"""
+Creation de l'indexage des vidéos 
+"""
+
+for title in os.listdir("../videos"):
+    hash = hashlib.md5()
+    hash.update(bytes(title.split(".")[0], "utf-8"))
+    path = os.getcwd().split("\\")
+    path.pop()
+    real_path = '\\'.join(path)+"/videos/"+title
+    INDEX_VIDEOS[hash.hexdigest()] = real_path
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(('', PORT))
 
+
+
 n = 0
 tabClients = dict()
+print(INDEX_VIDEOS)
+
+def padding(f1,f2):
+    size=0
+    dif = os.stat(f1).st_size-os.stat(f2).st_size
+    print(dif)
+    tab = bytearray(abs(dif))
+    for i in range(abs(dif)):
+        tab[i]=0
+    if dif == 0:
+        size=0
+    if dif >0:
+        size=os.stat(f2).st_size
+        file2 = open(f2, 'ab+')
+        file2.write(tab)
+        file2.close()
+    elif dif<0:
+        size=os.stat(f1).st_size
+        file1 = open(f1, 'ab+')
+        file1.write(tab)
+        file1.close()
+    return size
+
+def depadding(f,size):
+    fi=open(f,"rb+")
+    tab=fi.read()
+    temp=tab[:size]
+    fi.seek(0)
+    fi.truncate()
+    fi.write(temp)
+    fi.close()
+def couper(f,size):
+        fi=open(f,"rb+")
+        tab=fi.read()
+
+        temp=tab[size:]
+        fi.seek(0)
+        fi.truncate()
+        fi.write(temp)
+        fi.close()
+
+def encode(f1,f2,f3):
+        smallsize=padding(f1,f2)
+        s="%032d" % int(bin(smallsize)[2:])
+        tabsize=bytearray(s.encode())
+        size=os.stat(f1).st_size
+        result=bytearray(size)
+        with open(f1, 'rb') as file1:
+            with open(f2, 'rb') as file2:
+                with open(f3, 'wb') as file_out:
+                    fi1=bytearray(file1.read())
+                    fi2=bytearray(file2.read())
+                    for byte1 in range(size):
+                        result[byte1]=(fi1[byte1]^fi2[byte1])
+                    if smallsize == 0:
+                      tab=result
+                    else:
+                      tab=tabsize+result
+                    file_out.write(tab)
+                    print(os.stat(f3).st_size)
+                    return f3
+
+
 
 
 class ClientThread(threading.Thread):
@@ -35,24 +117,23 @@ class ClientThread(threading.Thread):
         if response != str():
             response = response.decode("utf-8")
             print(response)
-            files = os.listdir("../videos")
-            for eachFile in files:
-                if response in eachFile:
-                    FILE_FOUND = True
-                    binary_file = str()
-                    print(eachFile.split(".")[0])
-                    with open("../videos/" + eachFile, 'rb') as file_in:
-                        f = file_in.read(4096)
-                        while (f):
-                            clientsocket.send(f)
-                            f = file_in.read(4096)
-                    file = "key:" + eachFile.split(".")[0]
-                    clientsocket.send(bytes(file, "utf-8"))
-                    file_in.close()
-            if not FILE_FOUND:
-                clientsocket.send(b"File not found")
 
-        print("Client déconnecté...")
+            broadcast_answer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+            broadcast_answer.bind(('',BROADCAST_PORT))
+            # broadcast_answer.settimeout(1)
+            broadcast_answer.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            print(INDEX_VIDEOS[response])
+            with open(INDEX_VIDEOS[response], 'rb') as file_in:
+                f = file_in.read(4096)
+                while (f):
+                    broadcast_answer.sendto(f ,('255.255.255.255', 40000))
+                    f = file_in.read(4096)
+
+            file_in.close()
+            broadcast_answer.close()
+
+
+        # print("Client déconnecté...")
 
 
 try:
