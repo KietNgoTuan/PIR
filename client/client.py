8b21d231bd041fc1dcdca40c808a155d3d1fc44a
matthrx <matthieu_upmc@outlook.fr>
2019-05-29 11:11:28 +0200

    Multithread Server Adding caching placement (dir)

--- client/client.py
+++ client/client.py
@@ -1,25 +1,72 @@
 import socket as s
+import os
+import subprocess
+import sys
+import memcache
+import threading
+import tempfile
 
-HOST ="192.168.43.145" # Must be changed with the real server IP address
+HOST ="127.0.0.1" # Must be changed with the real server IP address
 PORT=25555
+DIR_TEMP_NAME = "PIRCaching"
+
 client= s.socket(s.AF_INET,s.SOCK_STREAM)
 client.connect((HOST,PORT))
-# cache = redis.Redis(host="127.0.0.1", port = 6379, db=0)
+client.settimeout(1)
+full_data = str()
 
-while True:
+
+if DIR_TEMP_NAME not in os.listdir(tempfile.gettempdir()):
+    print(tempfile.gettempdir())
+    os.mkdir(tempfile.gettempdir()+'/'+DIR_TEMP_NAME)
+    print("Done")
+
+tempfile.tempdir = tempfile.gettempdir()+"/"+DIR_TEMP_NAME
+print(tempfile.gettempdir())
+
+# try:
+#     def __run__cache():
+#         subprocess.call("..\memcached\memcached.exe")
+#
+#     thread = threading.Thread(target =__run__cache)
+#     thread.start()
+#
+# except subprocess.CalledProcessError:
+#     sys.exit(-1)
+#
+# print("Defining cache")
+# cache = memcache.Client(["127.0.0.1:11211"], debug=0)
+
+while(True):
     message = input("Fichier à télecharger : ")
-    # if cache.get(message) is not None:
-    #     print("Got it in cache")
+    if message == str():
+        print("Got it in cache")
+    else:
 
-    client.send(message.encode('utf-8'))
-    data=client.recv(4096)
+        client.send(message.encode('utf-8'))
+        data = client.recv(4096)
 
-    if(data):
-        data = data.decode('utf-8').split("+")
-        print(data[0].split(":")[1])
-        print(data[1].split(":")[1])
-        # cache.set(data[0].split(":")[1], data[1].split(":")[1])
-        # print(cache.get(data[0].split(":")[1]))
+        while (True):
+            full_data += str(data)
+            try:
+                data = client.recv(4096)
+            except s.timeout:
+                break
+
+
+        print("All data received")
+        print(full_data.split("key:")[1])
+        temp = tempfile.TemporaryFile(prefix=full_data.split("key:")[1], suffix="")
+        temp.write(bytes(full_data.split("key:")[0], "utf-8"))
+
+    # if(data.decode('utf-8') != "File not found" and data != str()):
+    #     data = data.decode('utf-8').split("+")
+    #     full_data += data[1].split(":")[1]
+    #     print(full_data)
+    #     print(data[0].split(":")[1])
+    #     print(data[1].split(":")[1])
+    #     cache.set(message, data[1].split(":")[1])
+    #     print(cache.get(message))
 
 
 
--- server/server.py
+++ server/server.py
@@ -1,44 +1,88 @@
 import socket
 import sys
 import os
+import threading
 from functools import partial
 
-NB_CLIENT  = 10
+NB_CLIENT = 10
 PORT = 25555
-FILE_FOUND = False
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.bind(('',PORT))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('', PORT))
 
-print("Start of server")
+n = 0
+tabClients = dict()
 
-while True:
-    try:
-        socket.listen(NB_CLIENT)
-        client, address = socket.accept()
-        print("{} connected".format(address))
-        response = client.recv(1024)
+
+class ClientThread(threading.Thread):
+    FILE_FOUND = False
+
+    def __init__(self, ip, port, clientsocket):
+
+        threading.Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.clientsocket = clientsocket
+        print("[+] Nouveau thread pour %s %s" % (self.ip, self.port,))
+
+    def run(self):  # cette fonction va gérer ce qu'on envoit et reçoit du client
+
+        print("Connexion de %s %s" % (self.ip, self.port,))
+
+        response = self.clientsocket.recv(4096)  # reçoit le message sur un buffer de 4096 bits
+
         if response != str():
             response = response.decode("utf-8")
             print(response)
-            files = os.listdir("../videos/")
+            files = os.listdir("../videos")
             for eachFile in files:
                 if response in eachFile:
                     FILE_FOUND = True
                     binary_file = str()
                     print(eachFile.split(".")[0])
-                    with open("../videos/"+eachFile, 'rb') as file_in:
-                        for byte in iter(partial(file_in.read, 1), b''):
-                           binary_file += format(ord(byte), '0>8b')
-                    file = "key:" + eachFile.split(".")[0]+"+value:"+binary_file
-                    client.send(bytes(file, "utf-8"))
+                    with open("../videos/" + eachFile, 'rb') as file_in:
+                        f = file_in.read(4096)
+                        while (f):
+                            clientsocket.send(f)
+                            f = file_in.read(4096)
+                    file = "key:" + eachFile.split(".")[0]
+                    clientsocket.send(bytes(file, "utf-8"))
+                    file_in.close()
             if not FILE_FOUND:
-                client.send(b"File not found")
+                clientsocket.send(b"File not found")
+
+        print("Client déconnecté...")
 
 
-    except KeyboardInterrupt:
-        print("End...")
-        socket.close()
-        sys.exit()
+try:
+    tabClients = {}
+    n = 0
+
+    while True:
+        s.listen(NB_CLIENT)
+        print("En écoute...")
+
+        (clientsocket, (ip, port)) = s.accept()  # on repere une connexion
+
+        tabClients[n] = clientsocket  # on rentre dans le dictionnaire des clients
+
+        newClientThread = ClientThread(ip, port, clientsocket)  # on lance un thread pour gérer le client
+        newClientThread.start()
+
+        n += 1
+
+
+except KeyboardInterrupt:
+    print('Exiting...')
+
+except:
+    for i in range(n):
+        tabClients[i].close()
+print('Je ferme les connexions')
+sys.exit()
+pass
+
+
 
 
