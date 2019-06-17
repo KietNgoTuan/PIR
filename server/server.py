781e1991ea1874cfc6726db8d058a543a266c072
matthrx <matthieu_upmc@outlook.fr>
2019-06-17 02:30:52 +0200

    ----------- Multiprocessing (Encode) -------------
    - Implementation of a multiprocessing encoding system (communication via QUEUE and manager object)
    - Tested into master and D2D communication
    - Final project
    - Large test before pull into master branch

--- server/server.py
+++ server/server.py
@@ -4,122 +4,12 @@ import os
 import json
 import threading
 import hashlib
-import shutil
-import copy
 import matrix.matrix as m
 import requests
 import mysql.connector # Must be manually installed (pip install mysql-connector)
 import time
+import multiprocessing
 
-NB_CLIENT = 10
-PORT = 25555
-BROADCAST_PORT = 44444
-COEFF_LAN = 1.05
-
-D2D_PORT_SRC = 45000
-D2D_PORT_DEST = [45454, 45460, 45464] # Un client peut gérer jusqu'à 3 D2D (MAX)
-
-INDEX_VIDEOS = dict()
-INDEX_REQUEST = list()
-CLIENTS_CACHE = dict()
-REQUIRED_FILES = dict() # for each file (hash) knows who asked for it
-MATRIX_CODAGE=list()
-deltat = float()
-ONGOING_REQUESTS = set()
-REQUEST_ORIGIN = dict()
-path_to_send = str()
-D2D_HOST = list()
-D2D_HOST_PORT = dict()
-
-
-PRIVATE_YOUTUBE_KEY = "AIzaSyDaKk0TDBSmnHSqmPXpmRCV2PApz8rJzqo"
-QUITTING = "7694f4a66316e53c8cdd9d9954bd611d"
-
-YOUTUBE_DICT = {
-    "BONGO" : "bHnuWN7z8gk",
-    "ALLAN" : "_dK2tDK9grQ",
-    "TOUR" :  "VcyFfcJbyeM",
-    "KALI" :  "7ysFgElQtjI",
-    "NYAN" :  "uKm2KN5gBiY"
-}
-
-SYNCHRONE_REQUEST = 3
-AMOUNT_CLIENT = int()
-
-# Initializing connection with mySQL
-try:
-    connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password = str()
-)
-
-    cursor = connection.cursor()
-except mysql.connector.errors:
-    print("Cannot reach database...")
-    sys.exit(0)
-
-mutex_clients_cache = threading.Lock()
-mutex_ongoing_request = threading.Lock()
-mutex_handle_client = threading.Lock()
-
-"""
-Creation de l'indexage des vidéos 
-"""
-
-cursor.execute("SELECT NOM FROM pir.videos;")
-all_names = [name for (name,) in cursor.fetchall()]
-for title in os.listdir("../videos"):
-    hash = hashlib.md5()
-    hash.update(bytes(title.split(".")[0], "utf-8"))
-    path = os.getcwd().split("\\")
-    path.pop()
-    real_path = '\\'.join(path)+"/videos/"+title
-    if title.split(".")[0] not in all_names:
-        popularity = requests.get("https://www.googleapis.com/youtube/v3/videos?part=statistics&id="
-                              +str(YOUTUBE_DICT[title.split(".")[0]])+"&key="+PRIVATE_YOUTUBE_KEY).content
-        json_popularity = json.loads(popularity)
-        parameters = (title.split(".")[0],hash.hexdigest(),
-                      (0.5*eval(json_popularity['items'][0]['statistics']['viewCount'])+
-                       eval(json_popularity['items'][0]['statistics']["likeCount"]))/
-                      (1.5*eval(json_popularity['items'][0]['statistics']['dislikeCount']))
-                      ,real_path)
-        request = "INSERT INTO PIR.VIDEOS (NOM,HASH_ID,POPULARITY,PATH) VALUES (%s,%s,%s,%s);"
-        cursor.execute(request, parameters)
-        connection.commit()
-
-    INDEX_VIDEOS[hash.hexdigest()] = real_path
-
-FILE_ID=list(INDEX_VIDEOS.keys())
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(('', PORT))
-
-
-n = 0
-tabClients = dict()
-print(INDEX_VIDEOS)
-
-def padding(f1,f2):
-    dif = os.stat(f2).st_size-os.stat(f1).st_size
-    tab = bytearray(abs(dif))
-    for i in range(abs(dif)):
-        tab[i]=0
-
-    file2 = open(f1 , 'ab+')
-    file2.write(tab)
-    file2.close()
-
-
-def depadding(f,size):
-    fi=open(f,"rb+")
-    tab=fi.read()
-    temp=tab[:size]
-    fi.seek(0)
-    fi.truncate()
-    fi.write(temp)
-    fi.close()
 
 def get_ideal_d2d(list_ip):
     """
@@ -149,49 +39,89 @@ def get_largest_file(all_files):
                 path = i
     return path, max
 
+
+def encode_data(size, result, ns, q):
+    i = 0
+    for byte in range(size):
+        for file in ns:
+            try:
+                result[byte] ^= file[i]
+            except IndexError:
+                ns.remove(file)
+        i +=1
+    q.put(result)
+
+
+def get_all_frag_threading(size):
+    ref = PROCESS_ENCODE+1
+    size_list = list()
+    value = 0
+    for i in range(ref):
+        if i == ref-1:
+            size_list.append(size)
+            return size_list
+        size_list.append(value)
+        value += int(size / ref)
+
+
+
 def encode(all_files, f3):
     """
     :param all_files: all files that must be XOR together
     :param f3: outfile
     :return: outfile
     """
+    t_debut = time.time()
     path , max_size = get_largest_file(all_files)
-    print(path)
-    print(max_size)
-    temp_list = copy.deepcopy(all_files)
-    print(temp_list)
-    temp_list.remove(path)
-    print(temp_list)
-    temp_file_list = list()
-
-    for each_path in range(len(temp_list)):
-        temp_file = os.getcwd()+"/temp_file/"+temp_list[each_path].split("/")[-1].split(".")[0]+"_temp.mp4"
-        temp_file_list.append(temp_file)
-        shutil.copyfile(temp_list[each_path], temp_file_list[each_path],  follow_symlinks=True)
-        padding(temp_file_list[each_path], path)
-
-    temp_file_list.append(path)
-    result = bytearray(max_size)
     list_file = list()
-    for each_path in temp_file_list:
+    for each_path in all_files:
         f = open(each_path, 'rb')
         list_file.append(f)
+    process_list = list()
+    bytelist = list()
+    result = bytearray(max_size)
+    manager = multiprocessing.Manager()
+    ns = manager.Namespace()
     with open(f3, 'wb') as file_out:
-        byte_list = list()
         for each_elt in range(len(all_files)):
-            byte_list.append(bytearray(list_file[each_elt].read()))
+            bytelist.append(bytearray(list_file[each_elt].read()))
+        frag = get_all_frag_threading(max_size)
+        print(frag)
+        ns.result = [bytearray(frag[i+1]-frag[i]) for i in range(0, len(frag)-1)]
+        q_list = [multiprocessing.Queue() for _ in range(0, len(frag)-1)]
+        i = int()
+        while i < len(frag)-1:
+            temp_list = list()
+            for elt in bytelist:
+                if len(elt) > frag[i]:
+                # _ = elt[frag[i+1]-1]
+                    temp_list.append(elt[frag[i]:frag[i+1]])
 
-        for byte1 in range(max_size):
-            for file in byte_list:
-                result[byte1] ^= file[byte1]
+            ns.bytelist = temp_list
+            p = multiprocessing.Process(
+                target = encode_data,
+                args = (frag[i+1]-frag[i],ns.result[i] ,ns.bytelist, q_list[i])
+            )
+            process_list.append(p)
+            p.start()
+            i += 1
+
+        for i in range(len(frag)-1):
+            if i==len(frag)-1:
+                result[frag[i]:max_size-1] = q_list[i].get()
+            else:
+                result[frag[i]:frag[i + 1]] = q_list[i].get()
+
+        print(len(result))
+        for p in process_list:
+            p.join()
+
         file_out.write(result)
-
     for f in list_file:
         f.close()
-    temp_file_list.pop()
-    print("Temp file lisr : {}".format(temp_file_list))
-    for file in temp_file_list:
-        os.remove(file)
+    print("Temps final : {}".format(time.time()-t_debut))
+
+
 
 def MatrixCodage(matrix,file):
     fichi=m.MatrixDifRequeste(matrix,file)
@@ -223,7 +153,7 @@ class ClientThread(threading.Thread):
 
         print("Connexion de %s %s" % (self.ip, self.port,))
         response = self.clientsocket.recv(4096)  # reçoit le message sur un buffer de 4096 bits
-        global SYNCHRONE_REQUEST, REQUIRED_FILES
+        global SYNCHRONE_REQUEST, REQUIRED_FILES, D2D_HOST_PORT
 
         while response.decode("utf-8").split("+")[0] != QUITTING:
             print("Start while : {}".format(response.decode("utf-8")))
@@ -362,11 +292,12 @@ class ClientThread(threading.Thread):
 
                                         if len(ip_dest) != 0:
                                             ip_dest = get_ideal_d2d(ip_dest)
-                                            port_dest = D2D_HOST_PORT[ip_dest][0]
-                                            print("ip_dest and port_dest : {},{}".format(ip_dest,port_dest))
-                                            D2D_HOST_PORT[ip_dest].pop(0)
+                                            if len(D2D_HOST_PORT[ip_dest]) != 0:
+                                                port_dest = D2D_HOST_PORT[ip_dest][0]
+                                                print("ip_dest and port_dest : {},{}".format(ip_dest, port_dest))
+                                                D2D_HOST_PORT[ip_dest].pop(0)
 
-                                if type(ip_dest) != list():
+                                if type(ip_dest) != list:
                                     cursor.execute("SELECT POPULARITY from pir.videos WHERE HASH_ID ='{}'"
                                                    .format(FILE_ID[each_coding[0]]))
                                     pop, = cursor.fetchall()
@@ -434,15 +365,18 @@ class ClientThread(threading.Thread):
                         deltat += inter_t
 
                 broadcast_answer.close()
+                SYNCHRONE_REQUEST = 2
+
                 del INDEX_REQUEST[:]
                 del MATRIX_CODAGE[:]
                 del index_files[:]
                 del index_rest[:]
-                del D2D_HOST[:] # Theorical
-                del D2D_HOST_PORT[:]
+                del D2D_HOST[:]  # Theorical
+                D2D_HOST_PORT = dict()
                 print(REQUIRED_FILES)
                 REQUIRED_FILES = dict()
-                SYNCHRONE_REQUEST = 3
+                print("Temps pris en seconde pour répondre à tout le monde : {}".format(deltat))
+                deltat = float()
                 print("Temps pris en seconde pour répondre à tout le monde : {}".format(deltat))
                 deltat = float()
             print("About to assign response")
@@ -450,28 +384,122 @@ class ClientThread(threading.Thread):
 
 
 
+if __name__ == "__main__":
+    NB_CLIENT = 10
+    PORT = 25555
+    BROADCAST_PORT = 44444
+    COEFF_LAN = 1.05
 
-while True:
+    D2D_PORT_SRC = 45000
+    D2D_PORT_DEST = [45454, 45460, 45464]  # Un client peut gérer jusqu'à 3 D2D (MAX)
+    PROCESS_ENCODE = multiprocessing.cpu_count()
+
+    INDEX_VIDEOS = dict()
+    INDEX_REQUEST = list()
+    CLIENTS_CACHE = dict()
+    REQUIRED_FILES = dict()  # for each file (hash) knows who asked for it
+    MATRIX_CODAGE = list()
+    deltat = float()
+    ONGOING_REQUESTS = set()
+    REQUEST_ORIGIN = dict()
+    path_to_send = str()
+    D2D_HOST = list()
+    D2D_HOST_PORT = dict()
+
+    PRIVATE_YOUTUBE_KEY = "AIzaSyDaKk0TDBSmnHSqmPXpmRCV2PApz8rJzqo"
+    QUITTING = "7694f4a66316e53c8cdd9d9954bd611d"
+
+    YOUTUBE_DICT = {
+        "GRAVE": "_yyjPxvNLGk",
+        "ALLAN": "_dK2tDK9grQ",
+        "YEUX": "FmUDe7P0fzg",
+        "ADIEU": "U4EICXeGtx0",
+        "CACHE": "hNK5izw6jUs",
+        "VERRA": "YltjliK0ZeA",
+        "SICKO": "6ONRf7h3Mdk",
+        "PSYCHO": "au2n7VVGv_c",
+        "BETTER": "UYwF-jdcVjY",
+        "TOUR": "WrsFXgQk5UI"
+    }
+
+    SYNCHRONE_REQUEST = 2
+    AMOUNT_CLIENT = int()
+
+    # Initializing connection with mySQL
     try:
-        s.listen(NB_CLIENT)
-        print("En écoute...")
-        (clientsocket, (ip, port)) = s.accept()  # on repere une connexion
-        REQUEST_ORIGIN[ip]=clientsocket
-        clientsocket.setblocking(True)
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password=str()
+        )
 
-
-        tabClients[n] = clientsocket  # on rentre dans le dictionnaire des clients
-        print("Client in it")
-        newClientThread = ClientThread(ip, port, clientsocket, mutex_handle_client)  # on lance un thread pour gérer le client
-        newClientThread.start()
-
-        AMOUNT_CLIENT += 1
-
-
-    except KeyboardInterrupt:
-        for i in range(n):
-            tabClients[i].close()
+        cursor = connection.cursor()
+    except mysql.connector.errors:
+        print("Cannot reach database...")
         sys.exit(0)
+
+    mutex_clients_cache = threading.Lock()
+    mutex_ongoing_request = threading.Lock()
+    mutex_handle_client = threading.Lock()
+
+    """
+    Creation de l'indexage des vidéos 
+    """
+
+    cursor.execute("SELECT NOM FROM pir.videos;")
+    all_names = [name for (name,) in cursor.fetchall()]
+    for title in os.listdir("../videos"):
+        hash = hashlib.md5()
+        hash.update(bytes(title.split(".")[0], "utf-8"))
+        path = os.getcwd().split("\\")
+        path.pop()
+        real_path = '\\'.join(path) + "/videos/" + title
+        if title.split(".")[0] not in all_names:
+            popularity = requests.get("https://www.googleapis.com/youtube/v3/videos?part=statistics&id="
+                                      + str(YOUTUBE_DICT[title.split(".")[0]]) + "&key=" + PRIVATE_YOUTUBE_KEY).content
+            json_popularity = json.loads(popularity)
+            parameters = (title.split(".")[0], hash.hexdigest(),
+                          (0.5 * eval(json_popularity['items'][0]['statistics']['viewCount']) +
+                           eval(json_popularity['items'][0]['statistics']["likeCount"])) /
+                          (1.5 * eval(json_popularity['items'][0]['statistics']['dislikeCount']))
+                          , real_path)
+            request = "INSERT INTO PIR.VIDEOS (NOM,HASH_ID,POPULARITY,PATH) VALUES (%s,%s,%s,%s);"
+            cursor.execute(request, parameters)
+            connection.commit()
+
+        INDEX_VIDEOS[hash.hexdigest()] = real_path
+
+    FILE_ID = list(INDEX_VIDEOS.keys())
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(('', PORT))
+
+    n = 0
+    tabClients = dict()
+    print(INDEX_VIDEOS)
+
+    while True:
+        try:
+            s.listen(NB_CLIENT)
+            print("En écoute...")
+            (clientsocket, (ip, port)) = s.accept()  # on repere une connexion
+            REQUEST_ORIGIN[ip]=clientsocket
+            clientsocket.setblocking(True)
+
+
+            tabClients[n] = clientsocket  # on rentre dans le dictionnaire des clients
+            print("Client in it")
+            newClientThread = ClientThread(ip, port, clientsocket, mutex_handle_client)  # on lance un thread pour gérer le client
+            newClientThread.start()
+
+            AMOUNT_CLIENT += 1
+
+
+        except KeyboardInterrupt:
+            for i in range(n):
+                tabClients[i].close()
+            sys.exit(0)
 
 
 
